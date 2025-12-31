a12b349d28324639a5766d316751fa207814a2f9
IliasHDZ <11091568+iliasHDZ@users.noreply.github.com>
2025-12-31 21:26:30 +0100

    webhook test

--- src/renderer/GroupManager.cpp
+++ src/renderer/GroupManager.cpp
@@ -121,7 +121,6 @@ void GroupManager::resetGroupStates() {
 
 void GroupManager::addGroupCombination(GroupCombination& comb, GroupCombinationIndex index) {
     groupCombinationIndicies[comb] = index;
-    
 
     for (auto groupId : comb.getSpan()) {
         usedGroupIds.insert(groupId);
